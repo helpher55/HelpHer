e3a837ee4fd2419d388e19d4b2660dc7aa49e033
allineo <alline.oliveira@gmail.com>
2019-03-27 12:09:31 -0400

    menu

--- static/js/menu.js
+++ static/js/menu.js
@@ -4,17 +4,6 @@
   });
 
 
- //---------------------------------------------------------
-  // Navbar
-  //---------------------------------------------------------
-  $('.NavbarToggle').on('click', function() {
-    $(this).toggleClass('On');
-    $('.NavbarLinkList, .NavbarMask, .ToggleOne, .ToggleTwo, .ToggleThree').toggleClass('On');
-  });
-
-  $('.NavbarLinkItem, .NavbarMask').on('click', function() {
-    $('.NavbarToggle, .NavbarLinkList, .NavbarMask, .ToggleOne, .ToggleTwo, .ToggleThree').removeClass('On');
-  });
 
 
  
